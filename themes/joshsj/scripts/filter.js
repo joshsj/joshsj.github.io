70561b43cee2f548fb4f7328f65dc625d6abbce7
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-07-28 16:10:14 +0100

    Tweaks

--- themes/joshsj/scripts/filter.js
+++ themes/joshsj/scripts/filter.js
@@ -18,12 +18,16 @@ hexo.extend.filter.register("after_post_render", function (data) {
 hexo.extend.filter.register("template_locals", async (locals) => {
   await Promise.allSettled(
     locals.site.data.series.map(async (s) => {
+      const f = hexo.extend.helper.get("format_url");
+
       s.description = (
         await hexo.render.render({
           text: s.description,
           engine: "md",
         })
       ).slice(3, -5); // remove p tags and \n
+
+      s.path = `series/${f(s.title)}`;
     })
   );
 });
