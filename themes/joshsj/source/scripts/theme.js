26409d00684c4bc8b4505b662b08af2b9abf427d
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2022-08-02 16:19:44 +0100

    Simplify theme toggle

--- themes/joshsj/source/scripts/theme.js
+++ themes/joshsj/source/scripts/theme.js
@@ -1,25 +1,21 @@
-const key = "themeMode";
-
-const toMode = (b) => ["light", "dark"][+b];
+const key = "DarkMode";
 
 const body = document.querySelector("body");
 const el = document.getElementById("theme-toggle");
 
-const update = (mode) => {
-  // toggle css class
-  body.classList[mode === "light" ? "remove" : "add"]("dark");
-  el.textContent = mode;
-  localStorage.setItem(key, mode);
+const update = (dark) => {
+  body.classList[dark ? "add" : "remove"]("dark");
+  el.textContent = ["light", "dark"][+dark];
+  localStorage.setItem(key, dark);
 };
 
+// bind to toggle el
+el.addEventListener("click", () => update(!body.classList.contains("dark")));
+
 // try storage, fallback to media query
-const initialMode =
-  localStorage.getItem(key) ||
-  toMode(matchMedia("(prefers-color-scheme: dark)").matches);
+const storedMode = localStorage.getItem(key);
+const initialMode = storedMode
+  ? storedMode === "true"
+  : matchMedia("(prefers-color-scheme: dark)").matches;
 
 update(initialMode);
-
-// bind to toggle el
-el.addEventListener("click", () =>
-  update(toMode(!body.classList.contains("dark")))
-);
