5448ee680c53a724ae393304ed141450f8d2b500
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2023-02-04 21:48:45 +0000

    Update urlFor

--- src/application/context/index.ts
+++ src/application/context/index.ts
@@ -1,2 +1,2 @@
 export * from "./types";
-export * from "./urlFor";
+export * from "./getHelpers";
--- src/application/context/urlFor.ts
+++ src/application/context/urlFor.ts
@@ -6,12 +6,7 @@ const urlFor = (context: SiteContext, transformers: Transformers): UrlFor => {
   const cache = new Map<Something, string>();
 
   return (kos, filename) => {
-    const something: Something | undefined =
-      typeof kos === "string"
-        ? // TODO ???
-          // @ts-ignore
-          data[kos].find((x) => x.file.name === filename)
-        : kos;
+    const something = typeof kos === "string" ? context.find((x) => x.file.name === filename) : kos;
 
     if (!something) {
       throw new Error(`urlFor failed with: ${kos}, ${filename}`);
@@ -22,7 +17,7 @@ const urlFor = (context: SiteContext, transformers: Transformers): UrlFor => {
     }
 
     const { full, sep } = transformers[something.category].location(something.file);
-    const url = full.startsWith(sep) ? full : sep + full;
+    const url = (full.startsWith(sep) ? full : sep + full).replace("index.html", "");
 
     return cache.set(something, url).get(something)!;
   };
