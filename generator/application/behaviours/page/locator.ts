ab8005eb540c1c3a96f96dbee55b57bcb364c26f
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2023-02-19 14:42:07 +0000

    Change page locator

--- generator/application/behaviours/page/locator.ts
+++ generator/application/behaviours/page/locator.ts
@@ -2,7 +2,8 @@ import { Locator } from "@application/behaviours/types";
 
 const pageLocator: Locator = (file) =>
   file.with({
-    segments: [],
+    segments: file.name !== "index" ? [file.name] : [],
+    name: "index",
     extension: ".html",
   });
 
