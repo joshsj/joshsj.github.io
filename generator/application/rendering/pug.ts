15c2196681e717f138140354279ab82d5db683fd
Joshua Sexton-Jones <joshua.sexton-jones@cmap.tech>
2023-02-20 19:12:59 +0000

    Tidy logging

--- generator/application/rendering/pug.ts
+++ generator/application/rendering/pug.ts
@@ -7,12 +7,11 @@ import { render } from "pug";
 import { Renderer, RenderHelpers } from "./types";
 
 // Garbage
-require("prismjs/components/index")(["typescript", "python", "csharp"]);
+require("prismjs/components/index")(["typescript", "python", "csharp", "json"]);
 
-type Lang = "ts" | "cs" | "py" | "tex";
+type Lang = "ts" | "cs" | "py" | "json" | "tex";
 type Options = { inline?: true };
 type Filter = (s: string, options: Options) => string;
-
 type Filters = { [L in Lang]: Filter };
 
 const highlight =
@@ -27,6 +26,7 @@ const filters: Filters = {
   cs: highlight("csharp"),
   py: highlight("python"),
   ts: highlight("typescript"),
+  json: highlight("json"),
   tex: (s, { inline }) => katex.renderToString(s, {
     displayMode: inline === undefined,
     throwOnError: true
